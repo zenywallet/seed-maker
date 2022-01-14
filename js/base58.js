5a90061b2b6797496d092d63984e89f6f25798d5
zenywallet <zenywallet@bitzeny.jp>
2022-01-15 01:23:02 +0900

    fix base58 first zero consecutive

--- js/base58.js
+++ js/base58.js
@@ -5,15 +5,6 @@ var base58 = (function(base58_chars) {
   for(var i in base58_chars) {
     base58_map[base58_chars[i]] = Number(i);
   }
-  var log58_256 = Math.log(256) / Math.log(58);
-  var log256_58 = 1 / log58_256;
-  function ceil_precision(x) {
-    var precision = String(x).replace('.', '').length - x.toFixed().length;
-    var multi = Number('1' + '0'.repeat(precision - 1));
-    return Math.ceil(x * multi) / multi;
-  }
-  log58_256 = ceil_precision(log58_256);
-  log256_58 = ceil_precision(log256_58);
 
   function obj_assign(obj, methods) {
     var o = Object.assign(obj, methods);
@@ -45,49 +36,63 @@ var base58 = (function(base58_chars) {
 
     enc: function(array) {
       array = array || this;
-      var size = Math.ceil(array.length * log58_256);
-      var buf = new Uint8Array(size);
-      var carry;
+      var enc = '';
       for(var i in array) {
-        carry = array[i];
-        for(var j = size - 1; j >= 0; j--) {
-          carry += buf[j] * 256;
-          buf[j] = carry % 58;
-          carry /= 58;
+        if(array[i] != 0) {
+          break;
+        }
+        enc += '1';
+      }
+      var b = [];
+      for(var i = enc.length; i < array.length; i++) {
+        var c = array[i];
+        var j = 0;
+        while(c > 0 || j < b.length) {
+          if(j >= b.length) {
+            b.push(0);
+          } else {
+            c += b[j] * 256;
+          }
+          b[j] = c % 58
+          c = Math.floor(c / 58);
+          j++;
         }
       }
-      var enc;
-      for(var i in buf) {
-        if(enc) {
-          enc += base58_chars[buf[i]];
-        } else if(buf[i] != 0) {
-          enc = base58_chars[buf[i]];
-        }
+      for(var i = b.length - 1; i >= 0; i--) {
+        enc += base58_chars[b[i]];
       }
       return obj_assign(new String(enc), methods);
     },
 
     dec: function(str) {
       str = str || this;
-      var size = Math.ceil(str.length * log256_58);
-      var dec = new Uint8Array(size);
-      var carry;
+      var zeroLen = 0;
       for(var i in str) {
-        carry = base58_map[str[i]];
-        if(carry == null) {
-          return null;
-        }
-        for(var j = size - 1; j >= 0; j--) {
-          carry += dec[j] * 58;
-          dec[j] = carry % 256;
-          carry /= 256;
-        }
-      }
-      for(var i in dec) {
-        if(dec[i] != 0) {
-          dec = dec.slice(i);
+        if(str[i] != '1') {
           break;
         }
+        zeroLen++;
+      }
+      var b = [];
+      for(var i = zeroLen; i < str.length; i++) {
+        var c = base58_map[str[i]];
+        if(c == null) {
+          return null;
+        }
+        for(var j = 0; j < b.length; j++) {
+          c += b[j] * 58;
+          b[j] = c % 256;
+          c = Math.floor(c / 256);
+        }
+        if(c > 0) {
+          b.push(c);
+        }
+      }
+      var dec = new Uint8Array(zeroLen + b.length);
+      var j = zeroLen;
+      for(var i = b.length - 1; i >= 0; i--) {
+        dec[j] = b[i];
+        j++;
       }
       return obj_assign(dec, methods);
     }
